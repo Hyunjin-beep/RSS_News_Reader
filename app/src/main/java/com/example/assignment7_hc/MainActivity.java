9cbb9aeb07a8b17881c371bed58de1b6d66ecddd
Hyunjin Cho <jhj176610@gmail.com>
2021-10-20 23:52:44 -0500

    Test for setting: Save the setting state and pass the information to MainActivity

--- app/src/main/java/com/example/assignment7_hc/MainActivity.java
+++ app/src/main/java/com/example/assignment7_hc/MainActivity.java
@@ -1,23 +1,29 @@
 package com.example.assignment7_hc;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.xml.sax.SAXException;
+import android.content.SharedPreferences;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -30,6 +36,13 @@ import javax.xml.parsers.SAXParserFactory;
 
 public class MainActivity<setOnClickListener> extends AppCompatActivity {
 
+    public static final String REQUEST_RESULT = "REQUEST_RESULT";
+    private  static final String KEY_PASSED = "PASSED";
+    private static final String KEY_TEXT = "TEXT";
+    private int passed = 0;
+    TextView tv_test;
+    EditText et_test;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +51,9 @@ public class MainActivity<setOnClickListener> extends AppCompatActivity {
 
         Toolbar myToolbar =  findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+
+        tv_test = findViewById(R.id.tv_test);
+        et_test = findViewById(R.id.et_test);
 
         View lv_one = findViewById(R.id.rl_one);
         lv_one.setOnClickListener(new View.OnClickListener() {
@@ -66,9 +82,34 @@ public class MainActivity<setOnClickListener> extends AppCompatActivity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+
+        SharedPreferences sp = getSharedPreferences("pref", Activity.MODE_PRIVATE);
+        boolean isCheckboxSet = sp.getBoolean("checkboxstate", false);
+        Toast.makeText(MainActivity.this, Boolean.toString(isCheckboxSet), Toast.LENGTH_SHORT).show();
+
+
+    }
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//
+//        SharedPreferences sharedPreferences = getSharedPreferences("Mysp", MODE_PRIVATE);
+//        SharedPreferences.Editor myedit = sharedPreferences.edit();
+//
+//        myedit.putString("test", et_test.getText().toString());
+//        myedit.apply();
+//    }
+
+    @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item){
         if(item.getItemId() == R.id.menu_setting){
             Toast.makeText(MainActivity.this, "clicked", Toast.LENGTH_SHORT).show();
+
+            Intent intent = new Intent(MainActivity.this, SettingDetails.class);
+            startActivity(intent);
         }
 
         return super.onOptionsItemSelected(item);
--- app/src/main/java/com/example/assignment7_hc/SettingDetails.java
+++ app/src/main/java/com/example/assignment7_hc/SettingDetails.java
@@ -1,14 +1,92 @@
 package com.example.assignment7_hc;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SettingDetails extends AppCompatActivity {
 
+    CheckBox cb_fin;
+    SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_details);
+
+        Toolbar myToolbar =  findViewById(R.id.my_toolbar_setting);
+        setSupportActionBar(myToolbar);
+
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
+        Intent intent = getIntent();
+
+        cb_fin = findViewById(R.id.cb_fin);
+
+        cb_fin.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // TODO Auto-generated method stub
+                if(isChecked){
+                    String settings = "fin";
+
+                    SharedPreferences preferences = getSharedPreferences("pref", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putBoolean("checkboxstate", Boolean.parseBoolean(Boolean.toString(isChecked)));
+                    editor.apply();
+
+
+                    Toast.makeText(SettingDetails.this, Boolean.toString(isChecked), Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
     }
+//    private View.OnClickListener saveData = new View.OnClickListener() {
+//        @Override
+//        public void onClick(View view) {
+//            String settings = et_settings.getText().toString();
+//
+//            SharedPreferences.Editor editor = sharedPreferences.edit();
+//            editor.putString("settings", settings);
+//            editor.apply();
+//
+//            Toast.makeText(SettingDetails.this, "saved", Toast.LENGTH_SHORT).show();
+//        }
+//    };
+
+
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // inflate the options menu
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_setting, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
 }
